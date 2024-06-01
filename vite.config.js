1159ddab43203e90f82954c944d43e4135b65049
Divyanshu6797 <divyanshu6797@gmail.com>
2024-06-01 09:44:14 +0530

    frontend warning fixed

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,9 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': ' http://localhost:3000/',  
-      //  https://social-media-backend-hq87.onrender.com/
+      '/api': 'https://social-media-backend-hq87.onrender.com/',  
+
+      //  http://localhost:3000/  
         
     },
   },
